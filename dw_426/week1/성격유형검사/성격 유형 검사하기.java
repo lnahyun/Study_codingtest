d29b25289ac403b9f040a3a709ee2a2d8a390ee2
dongwki2 <146003734+dw426@users.noreply.github.com>
2025-01-11 00:10:08 +0900

    Update 성격 유형 검사하기.java

--- dw_426/week1/성격유형검사/성격 유형 검사하기.java
+++ dw_426/week1/성격유형검사/성격 유형 검사하기.java
@@ -2,13 +2,14 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] survey, int[] choices) {
+        // 성격 유형, 점수 맴핑
         char[] type = {'R','T','C','F','J','M','A','N'};
         HashMap<Character,Integer> index = new HashMap<>();
         int score[] = new int[8];
-
         for(int i=0; i<8; i++)
             index.put(type[i],i);
 
+        // 설문 답변 처리 과정
         for(int i=0; i<survey.length; i++){
             char[] arr=survey[i].toCharArray();
 
@@ -29,6 +30,7 @@ class Solution {
                 default : break;
             }
         }
+        // 성격 유형 결정
         String answer = "";
         for(int i=0; i<8; i+=2){
             if(score[i]>score[i+1])
@@ -42,6 +44,7 @@ class Solution {
             else
                 answer+=type[i+1];
         }
+        // 결과 반환
         return answer;
     }
 }

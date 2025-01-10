77e24524722d423a0dce3ebea12869d1ec54c95b
dongwki2 <146003734+dw426@users.noreply.github.com>
2025-01-11 00:11:12 +0900

    Update 택배박스.java

--- dw_426/week1/택배상자/택배박스.java
+++ dw_426/week1/택배상자/택배박스.java
@@ -4,8 +4,10 @@ class Solution {
     public int solution(int[] order) {
         int idx = 0;
         Stack<Integer> stack = new Stack<>();
+        // 스택에 물건 쌓는 과정
         for (int i = 0; i < order.length; i++){
             stack.push(i+1);
+            // 스택에서 물건 꺼내기 과정
             while(!stack.isEmpty()){
                 if (stack.peek() == order[idx]){
                     stack.pop();
@@ -15,6 +17,7 @@ class Solution {
                     break;
             }
         }
+        // 최종 반환 값
         return idx;
     }
 }

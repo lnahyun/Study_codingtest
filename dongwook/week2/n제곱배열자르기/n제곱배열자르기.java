707a7c7f600359a1dd047257cda4ad0a1fd6faf4
dongwki2 <146003734+dw426@users.noreply.github.com>
2025-01-20 00:32:56 +0900

    Update n제곱배열자르기.java

--- dongwook/week2/n제곱배열자르기/n제곱배열자르기.java
+++ dongwook/week2/n제곱배열자르기/n제곱배열자르기.java
@@ -4,6 +4,7 @@ public class n제곱배열자르기 {
     public int[] solution(int n, long left, long right){
         int len = (int) right - (int) left; // 반환할 배열 길이 계산 -> 여기서 left, right가 long 타입이므로 int로 강제 형변환 과정이 이뤄저야함
         int[] answer = new int[len + 1]; // 결과값 배열을 초기화하는 단계
+        // len + 1크기의 배열을 생성하는 이유는 right, left가 주어지면 right - left + 1 크기의 요소를 가져야하니까!!
 
         int idx = 0; // 결과 배열 인덱스 0으로 초기화
         for (long i = left; i <= right; i++) {
@@ -11,7 +12,6 @@ public class n제곱배열자르기 {
             long col = i % n; // 열 번호 계산하는 과
             answer[idx++] = Math.max((int)row, (int)col) + 1; // 행과 열 중 큰 값에 1을 더해 배열에 저장하는 과정
         }
-// len + 1크기의 배열을 생성하는 이유는 right, left가 주어지면 right - left + 1 크기의 요소를 가져야하니까!!
         return answer; // 결과 배열 반환과
     }
 }

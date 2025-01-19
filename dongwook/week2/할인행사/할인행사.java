c6b6c2751dcd85082e017524a17bcb8ed2121a15
dongwki2 <146003734+dw426@users.noreply.github.com>
2025-01-20 01:29:58 +0900

    Update 할인행사.java

--- dongwook/week2/할인행사/할인행사.java
+++ dongwook/week2/할인행사/할인행사.java
@@ -5,19 +5,22 @@ class 할인행사 {
 
         int answer = 0;
 
-        for(int i=0; i<discount.length-9; i++) {
-            int cntday = 0;
+        for(int i=0; i<=discount.length-10; i++) { // i-> 할인 기간 시작 인덱스
+            int cntday = 0; // 원하는 제품이 모두 만족하는지
             for(int j=0; j<want.length; j++) {
-                int cnt = 0;
-                for(int k=i; k<i+10; k++) {
-                    if(discount[k].equals(want[j])) cnt++;
+                int cnt = 0; // 해당 제품의 할인된 개수
+                for(int k=i; k<i+10; k++) { // 10일동안 할인된 제품 확인
+                    if(discount[k].equals(want[j])) 
+                        cnt++; // 제품 찾으면 증가시켜줌
                 }
-                if(cnt<number[j]) break;
-                else cntday++;
+                if(cnt<number[j]) 
+                    break; // 수량이 안맞으면 탈출
+                else 
+                    cntday++; // 수량이 맞으면 증가
             }
-            if(cntday == want.length) answer++;
+            if(cntday == want.length) 
+                answer++; // 모든 제품을 원하는 만큼 맞춘 경우 answer 증가
         }
-
-        return answer;
+        return answer; // 유호한 할인 기간 개수 리턴
     }
 }

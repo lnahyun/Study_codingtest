090a0bfb8a3ed6e781cf1b9e174020621b7250cf
dongwki2 <146003734+dw426@users.noreply.github.com>
2025-01-20 00:51:11 +0900

    Update 소수만들기.java

--- dongwook/week2/소수만들기/소수만들기.java
+++ dongwook/week2/소수만들기/소수만들기.java
@@ -9,8 +9,8 @@ public class 소수만들기 {
         for (int i = 0; i < nums.length - 2; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
-                    int sum = nums[i] + nums[j] + nums[k];
-                    // 합이 소수인지 판별
+                    int sum = nums[i] + nums[j] + nums[k]; // 세 수의 합
+                    // 합이 소수인 경우 answer 값 증가시켜줌
                     if (isPrime(sum)) {
                         answer++;
                     }
@@ -25,11 +25,11 @@ public class 소수만들기 {
         if (number <= 1) {
             return false;
         }
-        for (int i = 2; i * i <= number; i++) {
+        for (int i = 2; i * i <= number; i++) { //2부터 number의 제곱근까지 나눠서 소수를 판별하는 과정이다
             if (number % i == 0) {
-                return false;
+                return false; // 나눠 떨어지면 false
             }
         }
-        return true;
+        return true; // 소수인 경우
     }
 }

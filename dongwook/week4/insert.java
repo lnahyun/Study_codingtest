14fce97435882b50cae170416e06b2476387f663
dongwki2 <146003734+dw426@users.noreply.github.com>
2025-02-02 18:56:54 +0900

    Update insert.java

--- dongwook/week4/insert.java
+++ dongwook/week4/insert.java
@@ -1,2 +1,32 @@
-package Study_codingtest.dongwook.week4;public class insert {
+package Study_codingtest.dongwook.week4;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    private Node first;
+
+    public LinkedList() {
+        this.first = null;
+    }
+
+    // insert 메서드를 LinkedList 클래스의 멤버 메서드로 이동
+    public void insert(Node x) {
+        Node temp = new Node(50);
+
+        if (first != null) {
+            temp.next = x.next;
+            x.next = temp;
+        } else {
+            temp.next = null;
+            first = temp;
+        }
+    }
 }

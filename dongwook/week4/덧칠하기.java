c0aaa3f56d4785596913405ebb245576ce4cfdd4
dongwki2 <146003734+dw426@users.noreply.github.com>
2025-02-02 19:02:08 +0900

    Update 덧칠하기.java

--- dongwook/week4/덧칠하기.java
+++ dongwook/week4/덧칠하기.java
@@ -5,17 +5,14 @@ class 덧칠하기 {
         int answer = 0;
         int idx = 0;
 
-        while (idx < section.length) {
-            int start = section[idx];
-            int end = start + m - 1;
-          
-            while (idx < section.length && section[idx] <= end) {
-                idx++;
+        while (idx < section.length) { // selection 배열을 순회하면서 덧칠 구간을 확인하는 while문
+            int start = section[idx]; // 시작점
+            int end = start + m - 1; // 끝점
+            while (idx < section.length && section[idx] <= end) { // 구간을 덧칠할 수 있는 동안
+                idx++; // idx 값 증가
             }
-
-            answer++;
+            answer++; // 덧칠 횟수 증가
         }
-
-        return answer;
+        return answer; // 최종 덧칠 횟수 반환
     }
 }

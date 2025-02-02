676a63c15e5aba3355a731b5da32372b08ab1ff3
dongwki2 <146003734+dw426@users.noreply.github.com>
2025-02-02 19:21:00 +0900

    Update 비밀지도.java

--- dongwook/week4/비밀지도/비밀지도.java
+++ dongwook/week4/비밀지도/비밀지도.java
@@ -6,17 +6,17 @@ class 비밀지도 {
         String[] arr = new String[arr1.length];
         int idx = 0;
         for (int i = 0; i < arr1.length; i++) {
-            int binary = arr1[i] | arr2[i];
-            String s = String.format("%" + n + "s", Integer.toBinaryString(binary)).replace(' ', '0');
-            arr[idx++] = s;
+            int binary = arr1[i] | arr2[i]; // 비트 or 연산과정 
+            String s = String.format("%" + n + "s", Integer.toBinaryString(binary)).replace(' ', '0'); // 이진수 문자열로 변환하고, n자리 맞추기 빈자리는 0
+            arr[idx++] = s; // 결과 arr 배열에 저장하는 과정
         }
-        int idx2 = 0;
+        int idx2 = 0; // answer 배열 인덱스
         for (int i = 0; i < arr.length; i++) {
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new StringBuilder(); // 문자열 조작을 위한 StringBuilder
             for (int j = 0; j < arr[i].length(); j++) {
-                if (arr[i].charAt(j) == '1') {
+                if (arr[i].charAt(j) == '1') { // 1이면 "#"
                     sb.append("#");
-                } else {
+                } else { // 0이면 " "공백
                     sb.append(" ");
                 }
             }

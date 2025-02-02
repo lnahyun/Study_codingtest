a40196d1d87b5c5e268f68b448e05831029e6dfd
dongwki2 <146003734+dw426@users.noreply.github.com>
2025-02-02 18:57:21 +0900

    Update 덧칠하기.java

--- dongwook/week4/덧칠하기.java
+++ dongwook/week4/덧칠하기.java
@@ -1,2 +1,21 @@
-package Study_codingtest.dongwook.week4;public class 덧칠하기 {
+package Study_codingtest.dongwook.week4;
+
+class 덧칠하기 {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int idx = 0;
+
+        while (idx < section.length) {
+            int start = section[idx];
+            int end = start + m - 1;
+          
+            while (idx < section.length && section[idx] <= end) {
+                idx++;
+            }
+
+            answer++;
+        }
+
+        return answer;
+    }
 }

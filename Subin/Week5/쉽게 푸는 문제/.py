5ca6ce9da3d133fd4c8893e88e368b19faf4e4f1
Hansubin <135015634+hansubsub@users.noreply.github.com>
2025-02-17 02:23:04 +0900

    Delete Subin/Week5 directory

--- Subin/Week5/쉽게 푸는 문제/.py
+++
@@ -1,9 +0,0 @@
-a,b= map(int,(input().split()))
-nums = []
-for i in range(46): #임의의 숫자를 넣어서 찾은 값임
-    for j in range(i+1):
-        nums.append(i+1)
-    if len(nums)>=1000:
-        break
-result = sum(nums[a-1:b])
-print(result)
